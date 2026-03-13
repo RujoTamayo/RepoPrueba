4ed8dd6e36f9670417b494f76a19360481da515d
Rujo <jocabed.tase27@gmail.com>
2026-03-12 22:53:48 -0600

    se agregó la funcion de calcular porcentaje de un parámetro, tarea de clase

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # RepoPrueba
+---
+
+Dentro del directorio /nodeJS existen ejercicios para ejecutar codigo JS, estos ejercicios se probaran con NODEJS. es decir, en la consola de la LapTop, NO EN EL NAVEGADOR
+
+Ejecucion:
+Abrir terminal cmd o powershell:
+
+```cmd
+node nombre_de_archivo.js
+```
--- nodeJS/ejercicios.js
+++ nodeJS/ejercicios.js
@@ -3,14 +3,18 @@
 // 1. Hola Mundo 
 function saludar(nombre) {
     console.log(`¡Hola, ${nombre}! You are pretty .... good :D !`);
+
 }
 
 // 2. Calcular de Edad Canina
 function edadLua(edadHumana) {
     const resultado = edadHumana * 7;
     console.log(`Lua tiene ${resultado} años perrunos.`);
-    console.log("Lua tiene " + resultado + " años perrunos.");
+
 }
+
+
+
 
 // 3. Inversor de Palabras
 function invertirTexto(texto) {
@@ -23,7 +27,7 @@ function invertirTexto(texto) {
 function listarFrutas() {
     const frutas = ["Sandía", "Uva", "Durazno", "Mango"];
     console.log("--- Frutas favoritas de Jok ---");
-    for (let i = 0; i < frutas.length; i++) {
+    for (let i = 0      ;       i < frutas.length      ;       i++) {
         console.log(`Fruta no. ${i + 1}. ${frutas[i]}`);
     }
 }
@@ -34,6 +38,8 @@ function filtrarMayores(lista) {
     const resultado = lista.filter(num => num > 10);
     console.log("Números mayores a 10:", resultado);
 }
+
+// Aqui va definida la funcion de la tarea extra, que reciba un numero y imprima el 25% de ese numero
 
 // ################################################################################
 
@@ -47,13 +53,28 @@ function calcularPrecioTotal(precio) {
 
 //  ----------------------------------------------- Ejecutar las funciones una por una:
 
-saludar("Jokita");
-// edadLua(5);
-// invertirTexto("JavaScript");
+// saludar("Lua" );
+// edadLua(7);
+// invertirTexto("Jokita es la mejor");
 // listarFrutas();
 // filtrarMayores([5, 12, 8, 20, 1]);
+// Aqui va la funcion
+
+calcularVeinticincoPorciento (100) 
+
+// Definir una funcion mas que reciba un parametro (numero) y que imprima el 25% de ese numero
+// Ejecuta la funcion en la consola
+// subir el ejercicio a github 
+// no subir el ejercicio a github hasta que hayas ejecutado la funcion en la consola y verificado que funciona correctamente
+
+function calcularVeinticincoPorciento(numero) {
+    const resultado = numero * 0.25;
+    console.log(`El 25% de ${numero} es: ${resultado}`);
+}   
+
+
+
 
 // Ejemplo interactivo del RETURN:
     // const factura = calcularPrecioTotal(100); 
     // console.log(`El total a pagar con IVA es: $${factura}`);
-
